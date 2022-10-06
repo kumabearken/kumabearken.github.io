b8edd7032088dce4fddde975b619dc05180f9dbf
kumabearken <kennykumagai@yahoo.com>
2022-10-07 00:50:18 +0900

    add sliders

--- js/main.js
+++ js/main.js
@@ -8,6 +8,36 @@ const aboutMe = document.getElementById("aboutMe");
 const contact = document.getElementById("contact");
 const reserve = document.getElementById("reserve");
 
+const swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+
 /* Toggle mobile menu */
 function toggleMenu() {
   if (menu.classList.contains("active")) {
@@ -82,3 +112,5 @@ indicators.forEach((indicator) => {
 });
 
 sections.forEach(onSectionLeavesViewport);
+
+ 
